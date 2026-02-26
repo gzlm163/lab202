aae5d31797dfb66f3f0726c8032ddc4009ca287c
gzlm163 <marusyagzl@gmail.com>
2026-02-26 16:07:41 +0700

    first addition

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class Animal
+{
+    public string Name;
+    public int Age;
+    public string Habitat;
+    public string DietType;
+    public string Color;
+    public double Weight;
+
+    protected Animal(string name, int age, string habitat, string dietType, string color, double weight)
+    {
+        Name = name;
+        Age = age;
+        Habitat = habitat;
+        DietType = dietType;
+        Color = color;
+        Weight = weight;
+    }
+
+    
